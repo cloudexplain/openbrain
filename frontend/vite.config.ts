4aff6b92ee8a5e61754c7c969dfd2733ec7ec1e9
Tobias Pitters <tobias.pitters@gmail.com>
2025-08-27 23:22:17 +0200

    add tiptap editor

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		include: [
+			'@tiptap/core',
+			'@tiptap/starter-kit',
+			'@tiptap/extension-link',
+			'@tiptap/extension-table',
+			'@tiptap/extension-task-list',
+			'@tiptap/extension-task-item'
+		]
+	}
 });
